638a592b60f8266b9ce06c449846971304ee3639
AP09770 <AP09770@SEG-E4022-02.ens.ad.etsmtl.ca>
2017-10-24 15:43:23 -0400

    Patient

--- TP2_Inf/src/Docteur.java
+++ TP2_Inf/src/Docteur.java
@@ -1,14 +1,13 @@
 
 /**
  * Cette classe regroupe tous les attributs et les méthodes pour la classe
- * docteur, les attributs sont son identification et son numéro de département.
- * Cette classe fait la description d'un docteur
+ * Docteur, les attributs sont son identification et son numéro de département.
+ * Cette classe fait la description d'un Docteur
  *
  * @author Long Tran & Benjamin Fontaine
  * @since (copyright) LTran & BFon - A2017
  * @version (copyright 2017)
  */
-
 /**
  *
  * @author AP02990 & AP09770
@@ -37,7 +36,7 @@ public class Docteur {
     public Docteur(Identification identification, int numDepartement) {
 
         // On copie les attributs de l'objet identification recu dans l'objet 
-        // Identification du docteur
+        // Identification du Docteur
         this.identification = identification;
         this.numDepartement = numDepartement;
 
@@ -45,16 +44,13 @@ public class Docteur {
 
     // Constructeur par copie d'objet, permet de copier les attributs de l'objet
     // passé en paramètre.
-    public Docteur(Docteur docteur) {
+    public Docteur(Docteur Docteur) {
 
-        // Copier les attributs de l'objet docteur passé en paramètre
-        this.identification.setNom(docteur.identification.getNom());
-        this.identification.setPrenom(docteur.identification.getPrenom());
-        this.numDepartement = docteur.numDepartement;
-      
+        // Copier les attributs de l'objet Docteur passé en paramètre
+        this(Docteur.identification.clone(), Docteur.numDepartement);
+
     }
 
-    
     // *********************ACCESSEURS******************************************
     // Permet de retourner l'objet identification
     public Identification getIdentification() {
@@ -62,28 +58,68 @@ public class Docteur {
     }
 
     // Permet de retourner numéro de département
-    public int getPrenom() {
+    public int getNumDepartement() {
         return this.numDepartement;
     }
 
-    
     // **********************MUTATEURS******************************************
-    // Permet de modifier l'identification du docteur
+    // Permet de modifier l'identification du Docteur
     public void setIdentification(Identification identification) {
 
-        // On change les valeurs du nom et prenom du docteur dans son objet 
+        // On change les valeurs du nom et prenom du Docteur dans son objet 
         // identification
         this.identification.setNom(identification.getNom());
         this.identification.setPrenom(identification.getPrenom());
     }
 
     // Permet de modifier le prénom au prénom passé en paramètre
-    public void setNumDepartement(int num) {
-        
-        // On change le numero de departement du docteur par celui passer 
+    public void setnumDepartement(int num) {
+
+        // On change le numero de departement du Docteur par celui passer 
         // en paramètre
         this.numDepartement = num;
     }
 
+    // Retourne la concaténation du nom et du prénom et du numéro de departement.
+    public String toString() {
+
+        // On fait appel au toString de identification et on ajoute le n. de
+        // departement du Docteur
+        return (identification.toString() + numDepartement);
+
+    }
+
+    // Permet de comparer 2 objets du même type
+    public boolean equals(Docteur docteur) {
+
+        // Comparer les adressses de référence
+        if (this == docteur) {
+
+            return true;
+
+        }
+
+        // Comparer les attributs des objets
+        if (identification.getNom().equals(docteur.identification.getNom())
+                && identification.getPrenom().equals(docteur.identification
+                        .getPrenom()) && numDepartement == docteur.numDepartement) {
+
+            return true;
+
+        } // Si l'adresse ou que les attributs ne sont pas pareils.
+        else {
+
+            return false;
+
+        }
     
+    }
+    
+    // Permet d'obtenir une instance de la classe avec une copie des attributs
+    // de l'objet passé en paramètre    
+    public Docteur clone() {
+        
+        // On retourne le clone de l'objet
+        return new Docteur(this);
+    }
 }
